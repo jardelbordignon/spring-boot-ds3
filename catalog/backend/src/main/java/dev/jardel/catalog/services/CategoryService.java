adea5f1126d3b5a66e2eb438672b30d27235e801
Jardel Bordignon <jardel1101@gmail.com>
2024-04-28 16:56:25 -0300

    :sparkles: feat: add @Transactional annotation

--- catalog/backend/src/main/java/dev/jardel/catalog/services/CategoryService.java
+++ catalog/backend/src/main/java/dev/jardel/catalog/services/CategoryService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
@@ -14,6 +15,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true) // Read only transaction to avoid concurrency problems in the database access (only read operations)
     public List<Category> findAll() {
         return repository.findAll();
     }
