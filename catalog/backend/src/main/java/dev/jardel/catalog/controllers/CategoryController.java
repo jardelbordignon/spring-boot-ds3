e3decd03352cd39e8591e71bf0e35fb6f5d7cd96
Jardel Bordignon <jardel1101@gmail.com>
2024-04-28 20:16:23 -0300

    :sparkles: feat: using DTOs - GetCategoryDto GetCategoriesDto

--- catalog/backend/src/main/java/dev/jardel/catalog/controllers/CategoryController.java
+++ catalog/backend/src/main/java/dev/jardel/catalog/controllers/CategoryController.java
@@ -1,14 +1,14 @@
 package dev.jardel.catalog.controllers;
 
-import java.util.List;
-
+import dev.jardel.catalog.dto.GetCategoriesDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.jardel.catalog.dto.CategoryDto;
+import dev.jardel.catalog.dto.GetCategoryDto;
 import dev.jardel.catalog.services.CategoryService;
 
 @RestController
@@ -19,9 +19,15 @@ public class CategoryController {
   private CategoryService service;
 
   @GetMapping
-  public ResponseEntity<List<CategoryDto>> findAll() {
-    List<CategoryDto> categories = service.findAll();
-    return ResponseEntity.ok().body(categories);
+  public ResponseEntity<GetCategoriesDto> findAll() {
+    GetCategoriesDto dto = service.findAll();
+    return ResponseEntity.ok().body(dto);
+  }
+
+  @GetMapping(path = "/{id}")
+  public ResponseEntity<GetCategoryDto> findById(@PathVariable Long id) {
+    GetCategoryDto dto = service.findById(id);
+    return ResponseEntity.ok().body(dto);
   }
 
 }
--- catalog/backend/src/main/java/dev/jardel/catalog/services/CategoryService.java
+++ catalog/backend/src/main/java/dev/jardel/catalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package dev.jardel.catalog.services;
 
 import dev.jardel.catalog.dto.CategoryDto;
+import dev.jardel.catalog.dto.GetCategoriesDto;
+import dev.jardel.catalog.dto.GetCategoryDto;
 import dev.jardel.catalog.entities.Category;
 import dev.jardel.catalog.repositories.CategoryRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,9 +20,21 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true) // Read only transaction to avoid concurrency problems in the database access (only read operations)
-    public List<CategoryDto> findAll() {
+    public GetCategoriesDto findAll() {
         List<Category> categories = repository.findAll();
         // List<CategoryDto> categoryDtos = categories.stream().map(cat -> new CategoryDto(cat)).toList();
-        return categories.stream().map(CategoryDto::new).toList();
+        List<CategoryDto> categoryDtos = categories.stream().map(CategoryDto::new).toList();
+        return new GetCategoriesDto(categoryDtos);
+    }
+
+    @Transactional(readOnly = true)
+    public GetCategoryDto findById(Long id) {
+        Optional<Category> categoryObj = repository.findById(id);
+        if (categoryObj.isEmpty()) {
+            throw new IllegalArgumentException("Category not found with id: " + id);
+        }
+
+        CategoryDto categoryDto = new CategoryDto(categoryObj.get());
+        return new GetCategoryDto(categoryDto);
     }
 }
